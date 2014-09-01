7747568dcc0a82b94b147d625b4e90ad0ec1d804
ron crosler <rynclsnr@users.noreply.github.com>
2014-09-01 12:30:58 -0400

    Update sucker.rb

--- config/initializers/sucker.rb
+++ config/initializers/sucker.rb
@@ -1,6 +1,6 @@
 Sucker.configure do |config|
-  config.key              = "AKIAJAMX5IXWWESXVREA"
-  config.secret           = "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"
-  config.associate_tag    = "sb05e-20"
+  config.key              = ENV["AWS_ACCESS_KEY"]
+  config.secret           = ENV["AWS_SECRET_KEY"]
+  config.associate_tag    = ENV["AWS_ASSOCIATE_TAG"]
   config.locale           = "us"
 end
